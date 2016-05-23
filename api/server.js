b56dbb459e5af9c4398adf63de14c3d77d99632e
Nicolas Arroyo <nicoarroyoes@gmail.com>
2016-05-23 15:46:58 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -3,8 +3,16 @@ var bodyParser = require('body-parser')
 var fs = require('fs');
 
 var app = express();
-app.use(bodyParser.json());
 var cache;
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 function read() {
   return JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf8'));
@@ -19,21 +27,7 @@ app.listen(8000);
 app.get('/users', function(req, res) {
   console.log('GET /users');
   cache = read();
-  res.header("Access-Control-Allow-Origin","*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.json(cache.users);
-});
-
-
-app.options('/user', function(req, res) {
-  var headers = {};
-  headers["Access-Control-Allow-Origin"] = "*";
-  headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-  headers["Access-Control-Allow-Credentials"] = false;
-  headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-  headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
-  res.writeHead(200, headers);
-  res.end();
 });
 
 
@@ -42,9 +36,6 @@ app.post('/user', function(req, res) {
   cache = read();
   console.log('POST /user');
   var user = req.body;
-
-  res.header("Access-Control-Allow-Origin","*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
   var validation = {
     firstname: "string",
